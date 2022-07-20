14031c23ee52f5dd8f47dc56072ce0d12ad0d3d9
Gültekin Ünal <36562822+cinnetcrash@users.noreply.github.com>
2022-07-20 12:31:12 +0300

    Update triangle.sh

--- Script_examples/triangle.sh
+++ Script_examples/triangle.sh
@@ -1,20 +1,31 @@
-echo "Enter First number"
+#!/bin/bash
+
+# Kullanıcıdan ilk kenar değeri alınır.
+echo "Birinci numarayı giriniz: "
 read a
-echo "Enter Second Number"
+
+# Kullanıcıdan ikinci kenar değeri alınır.
+echo "İkinci numarayı giriniz: "
 read b
-echo "Enter Last Number"
+
+# Kullanıcıdan üçüncü kenar değeri alınır.
+echo "Son numarayı giriniz: "
 read c
 
 
+
+# If yapıları kullanılarak bu kenarlar birbirleri ile karşılaştırılır. 
+# Bütün kenarların birbirine eşit olma durumu kontrol edilir.
 if [ $a == $b -a $b == $c -a $a == $c ]
 then 
-echo Equilateral
-
+echo "Eşkenar Üçgen"
+# En az iki kenarın eşit olması durumu denetlenir.
 elif [ $a == $b -o $b == $c -o $a == $c ]
 then
-echo Isosceles
+echo "İkizkenar Üçgen"
 
+# Yukarıdaki koşulların sağlanmadığı durumlarda hiçbir kenarın birbiri ile uyuşmaması durumu
 else
-echo Scalene
+echo "Çeşitkenar Üçgen"
 
 fi
